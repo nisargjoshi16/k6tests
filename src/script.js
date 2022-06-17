5d438b035cf5dd1ae8af6499ab372ffcad55dd64
Nisarg Joshi <nisarg.joshi@allscripts.com>
2022-06-17 17:02:28 +0530

    AKS Integration

--- src/script.js
+++ src/script.js
@@ -1,4 +1,4 @@
-import http from 'k6/http';
+/* import http from 'k6/http';
 import { sleep } from 'k6';
 
 export const options = {
@@ -8,4 +8,22 @@ export const options = {
 export default function () {
   http.get('http://test.k6.io');
   sleep(1);
+}
+ */
+
+import http from 'k6/http';
+import { check } from 'k6';
+
+export const options = {
+  stages: [
+    { target: 10, duration: '5s' },
+    { target: 0, duration: '5s' },
+  ],
+};
+
+export default function () {
+  const result = http.get('https://test-api.k6.io/public/crocodiles/');
+  check(result, {
+    'http response status code is 200': result.status === 200,
+  });
 }
